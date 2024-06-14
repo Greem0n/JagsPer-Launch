19cd2b38c4f4994e505f4143cc1efc33d75a79fd
Greem0n <nacho213.f1.h4@gmail.com>
2024-06-15 00:06:09 +0200

    Basic text for the landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'JagsPer';
+
+  constructor(private renderer: Renderer2) {}
+
+  // @ViewChild('dotsElement') dotsElement!: ElementRef;
+
+  ngOnInit(): void {
+    // this.startAnimation();
+  }
+
+  // startAnimation(): void {
+  //   console.log('esta entrando');
+  //   const dotsArray = this.dotsElement.nativeElement.textContent;
+  //   for (let i = 0; i < dotsArray.length; i++) {
+  //     if (i % 2 == 0) {
+  //       this.dotsElement.nativeElement.textContent += '.';
+  //     }
+  //     const newSpan = this.renderer.createElement('span');
+  //     const text = this.renderer.createText('.');
+  //     this.renderer.appendChild(newSpan, text);
+  //     this.renderer.addClass(newSpan, 'dot_moving');
+  //     setTimeout(() => {}, 500);
+  //     this.renderer.appendChild(this.dotsElement.nativeElement, newSpan);
+  //   }
+  // }
 }
