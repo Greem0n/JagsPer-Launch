4913f91aae9cb258f8f0db3b9c5a60c102d38219
Greem0n <nacho213.f1.h4@gmail.com>
2024-07-11 22:08:36 +0200

    Multilanguage configurations. English or Spanish at this moment. Showing the inactive language's button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,27 +8,7 @@ import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 export class AppComponent {
   title = 'JagsPer';
 
-  constructor(private renderer: Renderer2) {}
+  constructor() {}
 
-  // @ViewChild('dotsElement') dotsElement!: ElementRef;
-
-  ngOnInit(): void {
-    // this.startAnimation();
-  }
-
-  // startAnimation(): void {
-  //   console.log('esta entrando');
-  //   const dotsArray = this.dotsElement.nativeElement.textContent;
-  //   for (let i = 0; i < dotsArray.length; i++) {
-  //     if (i % 2 == 0) {
-  //       this.dotsElement.nativeElement.textContent += '.';
-  //     }
-  //     const newSpan = this.renderer.createElement('span');
-  //     const text = this.renderer.createText('.');
-  //     this.renderer.appendChild(newSpan, text);
-  //     this.renderer.addClass(newSpan, 'dot_moving');
-  //     setTimeout(() => {}, 500);
-  //     this.renderer.appendChild(this.dotsElement.nativeElement, newSpan);
-  //   }
-  // }
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LanguageSelectorComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslocoRootModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
